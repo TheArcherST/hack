1a3f46f4cc6762c79ea268c4fa30b72f2fbc93c6
TheArcherST <mihailsapovalov05@gmail.com>
2025-10-25 00:51:32 -0700

    Upd

--- python/src/hack/core/models/check_implementations/base.py
+++ python/src/hack/core/models/check_implementations/base.py
@@ -5,8 +5,12 @@ from abc import abstractmethod
 
 from pydantic import BaseModel
 
+from .types import CheckTaskTypeEnum
+
 
 class BaseCheckTaskPayload(BaseModel, abc.ABC):
+    type: CheckTaskTypeEnum
+
     @abstractmethod
     async def perform_check(self) -> BaseCheckTaskResult:
         raise NotImplementedError
--- python/src/hack/core/models/check_implementations/dns.py
+++ python/src/hack/core/models/check_implementations/dns.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
+from typing import Literal
+
 from pydantic import IPvAnyAddress
 
 from .base import BaseCheckTaskPayload, BaseCheckTaskResult
+from .types import CheckTaskTypeEnum
 
 
 class DNSCheckTaskPayload(BaseCheckTaskPayload):
+    type: Literal[CheckTaskTypeEnum.DNS] = CheckTaskTypeEnum.DNS
+
     ip: IPvAnyAddress
     # and other fields...
 
