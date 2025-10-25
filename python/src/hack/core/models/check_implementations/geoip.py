e66fbcc88c3a036801a75ba3ccd6d6cfac8443b2
TheArcherST <mihailsapovalov05@gmail.com>
2025-10-25 15:51:08 -0700

    Upd

--- python/src/hack/core/models/check_implementations/geoip.py
+++ python/src/hack/core/models/check_implementations/geoip.py
@@ -45,7 +45,7 @@ class GeoIPCheckTaskPayload(BaseCheckTaskPayload):
         if "error" in data:
             return GeoIPCheckTaskResult(error=data["error"])
 
-        return GeoIPCheckTaskResult.model_validate(data)
+        return GeoIPCheckTaskResult(**data)
 
 
 class GeoIPCheckTaskResult(BaseCheckTaskResult):
