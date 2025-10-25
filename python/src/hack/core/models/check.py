596d0cf355a191e5270a467d8a3faa4a17e6cae0
TheArcherST <mihailsapovalov05@gmail.com>
2025-10-25 13:56:51 -0700

    Fix

--- python/src/hack/core/models/check.py
+++ python/src/hack/core/models/check.py
@@ -3,8 +3,9 @@ from datetime import datetime
 from uuid import UUID
 
 from sqlalchemy import JSON, ForeignKey
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
+from hack.core.models import CheckTask
 from hack.core.models.base import Base, CreatedAt
 
 
@@ -17,3 +18,5 @@ class Check(Base):
     payload: Mapped[dict] = mapped_column(JSON)
 
     created_at: Mapped[CreatedAt]
+
+    tasks: Mapped[list[CheckTask]] = relationship(lazy="selectin")
