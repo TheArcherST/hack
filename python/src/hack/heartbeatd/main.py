3014c1e49ed2b17a08b13033a30d573b5844ea4b
TheArcherST <mihailsapovalov05@gmail.com>
2025-10-25 12:27:13 -0700

    Upd

--- python/src/hack/heartbeatd/main.py
+++ python/src/hack/heartbeatd/main.py
@@ -42,21 +42,23 @@ async def heartbit(
 ):
     print("Enter heartbit check")
     connector = await agent_service.get_connector(agent_id)
-    async with connector.connect(timeout=3) as conn:
-        try:
+    try:
+        async with connector.connect(timeout=3) as conn:
             await conn.options("/", timeout=3)
             # todo: heartbit endpoint
-        except Exception as e:
-            print(f"Error while trying to connect to agent {agent_id}: `{e}`")
-            status = AgentStatus.DOWN
-        else:
-            status = AgentStatus.UP
-        print(f"Status of agent {agent_id} is set to {status}")
-        await agent_service.heartbit_mark(
+    except Exception as e:
+        print(f"Error while trying to connect to agent {agent_id}: `{e}`")
+        status = AgentStatus.DOWN
+    else:
+        status = AgentStatus.UP
+
+    await agent_service.heartbit_mark(
             agent_id=agent_id,
             status=status,
-        )
-        await uow_ctl.commit()
+    )
+
+    print(f"Status of agent {agent_id} is set to {status}")
+    await uow_ctl.commit()
 
 
 @broker.task(schedule=[{"cron": "*/1 * * * *"}])
