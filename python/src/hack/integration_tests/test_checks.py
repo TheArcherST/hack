dd70d8e9df53a95ef7737de35f44c1d260c84a39
TheArcherST <mihailsapovalov05@gmail.com>
2025-10-26 02:20:31 -0700

    Fixup

--- python/src/hack/integration_tests/test_checks.py
+++ python/src/hack/integration_tests/test_checks.py
@@ -58,3 +58,16 @@ def test_streams(
     }
     r = client.prepsend(req)
     assert r.status_code == 201
+
+
+    req = api_templates.make_create_check()
+    req.json = {
+        "payload": {
+            "type": "tcp_and_udp",
+            "url": "gmail.com",
+            "protocol": "udp",
+            "port": 80,
+        }
+    }
+    r = client.prepsend(req)
+    assert r.status_code == 201
